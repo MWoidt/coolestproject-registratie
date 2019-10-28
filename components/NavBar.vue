f058bbadf4601ed5ca5606af27ca06e96f5cd097
Michael Woidt <michael.woidt@gmail.com>
2019-10-28 23:59:49 +0100

    navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg">
+            <b-navbar-brand href="/">Coolest Projects</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item href="/registration">Registratie</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item-dropdown text="Kies je taal" right>
+                    <b-dropdown-item href="#">English</b-dropdown-item>
+                    <b-dropdown-item href="#">Francais</b-dropdown-item>
+                    <b-dropdown-item href="#">Nederlands</b-dropdown-item>
+                </b-nav-item-dropdown>
+
+                <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template v-slot:button-content>
+                    Welkom <em>test</em>
+                </template>
+                <b-dropdown-item href="#">Mijn project</b-dropdown-item>
+                <b-dropdown-item href="#">Log uit</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,26 @@
 <template>
-  <div class="container">
-    <div>
-      <img src="~/assets/CPlogo.png" width="200" height="160">
-      <h1 class="title">
-        <img src="~/assets/coderdojo.png" width="400" height="70">
-      </h1>
-      <h2 class="subtitle">
-        Coolest Project Registration Procedure 2020
-      </h2>
-      <div class="links">
-        <n-link class="button--grey" to="/registration">
-          Start registratie
-        </n-link>
-      </div>
-    </div>
+  <div>
+    <b-row>
+      <b-col>
+        <b-jumbotron header="BootstrapVue" lead="Coolest Project Registration Procedure 2020">
+          <template v-slot:header>
+            <b-img center src="~/assets/CPlogo.png" width="200" height="160" />
+          </template>
+          <b-button variant="success" href="/registration">Start registratie</b-button>
+          <h2 class="text-center">georganiseerd door:</h2>
+          <b-img center src="~/assets/coderdojo.png" width="400" height="70" />
+        </b-jumbotron>
+      </b-col>
+    </b-row>
   </div>
 </template>
 <script>
+export default {
+  data () {
+    return {}
+  }
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
