7f94d582e07303a768d3d1a4c6e0b56590f5fe7d
Michael Woidt <michael.woidt@gmail.com>
2019-11-01 19:50:00 +0100

    logo change

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,24 @@
   <div>
     <b-jumbotron class="bg-info">
       <template v-slot:header>
-        <span class="hero-text">Belgium</span>
+      <img
+              width="110"
+              src="~/assets/coderdojo3.png"
+              class="img-fluid"
+              alt="Coolest Projects3"
+              itemprop="logo"
+      >
+        <span class="hero-text">Coderdojo Belgium </span>
       </template>
       <template v-slot:lead>
-        Coolest Project Registration Procedure 2020
+        Coolest Projects Registration Procedure 2020
       </template>
       <b-button variant="dark" class="button-hero" href="/registration">
         <font-awesome-icon :icon="['fas', 'paper-plane']" /> Start registratie
       </b-button>
     </b-jumbotron>
     <b-container>
-      <h1>Coolest project Belgium</h1>
+      <h1>Coolest projects Belgium</h1>
       <p>
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -23,7 +30,7 @@
       </p>
     </b-container>
     <b-row>
-      <b-col>aa</b-col>
+      <b-col>hier komt nog wat denk ik</b-col>
     </b-row>
   </div>
 </template>
