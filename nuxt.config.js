f046b8f4c831923a2f69f12e8a6e139aa783f96f
Erik Woidt <erik@woidt.be>
2019-11-03 11:07:41 +0100

    logo & favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,42 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'msapplication-TileColor', content: '#000000' },
+      { name: 'theme-color', content: '#ffffff' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: 'apple-touch-icon.png'
+      },
+      {
+        rel: 'icon',
+        types: 'image/png',
+        sizes: '32x32',
+        href: 'favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        types: 'image/png',
+        sizes: '16x16',
+        href: 'favicon-16x16.png'
+      },
+      {
+        rel: 'manifest',
+        href: 'site.webmanifest'
+      },
+      {
+        rel: 'mask-icon',
+        color: '#5bbad5',
+        href: 'safari-pinned-tab.svg'
+      }
     ]
   },
   /*
